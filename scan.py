c57f9dd6aca87cb87e52fd7a4a8748da93c5446b
Rodrigue Ntagashobotse <112996908+RNtag12@users.noreply.github.com>
2024-07-01 22:03:06 +1000

    scan.py

--- scan.py
+++ scan.py
@@ -1,3 +1,6 @@
+"""
+DISCLAIMER: This code is intended for educational purposes only. The author does not condone or support any illegal or unethical activities, including unauthorized access to computer systems. The user of this code is solely responsible for its use. By using this code, you agree to use it responsibly and in compliance with all applicable laws and regulations.
+"""
 from scapy.all import *
 
 ip = "172.168.0.1"
