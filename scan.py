8abb01b3a8c1c3e116fcce2d3ae244758874cf2d
Rodrigue Ntagashobotse <112996908+RNtag12@users.noreply.github.com>
2024-07-03 22:59:35 +1000

    scan.py

--- scan.py
+++ scan.py
@@ -4,4 +4,39 @@ DISCLAIMER: This code is intended for educational purposes only. The author does
 from scapy.all import *
 
 ip = "172.168.0.1"
-ports=[53,80]
+#ports list is made of ports that are assumed to be running services which means that anyone
+#attempting to connect to a different port is potentially malicious and will be listed in blocked
+ports=[53,80] 
+honeyports =[8080,8443]
+
+blocked = []
+
+def analyzePacekts(p):
+  global blocked
+  if p.haslayer(IP):
+    response= Ether(src=p[Ether].dst,dst=p[Ether].src) /\ IP(src=p[IP].dst,dst=p[IP].src) /\ TCP (sport=p[TCP].dport,dport=p[TCP].sport,ack=p[TCP].seq+1)
+    source = p[IP].src
+  else:
+    response= Ether(src=p[Ether].dst,dst=p[Ether].src) /\ IPv6(src=p[IPv6].dst,dst=p[IPv6].src) /\ TCP (sport=p[TCP].dport,dport=p[TCP].sport,ack=p[TCP].seq+1)
+    source = p[IPv6].src
+  if p[TCP].flags != "S":
+    return
+  port = p[TCP].dport 
+  if source in blocked:
+    if port in ports:
+      response[TCP].flags = "RA"
+      print ("Sending reset")
+    elif port in honeyports:
+      reponse[TCP].flags = "SA"
+    else:
+      return
+    sendp(response, verbose=False)
+  else:
+    if port not in ports:
+      blocked += [source]
+      if port in honeports:
+        response[TCP].flags = "SA"
+        sendp(respone, verbose=False)
+
+f = "dst host "+ip+" and tcp"
+sniff(filter=f, prn=analyzePackets)
